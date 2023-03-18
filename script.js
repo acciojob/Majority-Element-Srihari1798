dc8e01cf676886053781da14814c53c7046bfc25
Srihari1798 <120098197+Srihari1798@users.noreply.github.com>
2023-03-18 20:25:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 //your code here
+// function majorityElement(nums) {
+//   let count = 0;
+//   let candidate = null;
+
+//   for (let num of nums) {
+//     if (count === 0) {
+//       candidate = num;
+//     }
+
+//     count += (num === candidate) ? 1 : -1;
+//   }
+
+//   return candidate;
+// }
 function majorityElement(nums) {
-  let count = 0;
-  let candidate = null;
-
-  for (let num of nums) {
-    if (count === 0) {
-      candidate = num;
+  const countMap = {};
+  const n = nums.length;
+  
+  for (let i = 0; i < n; i++) {
+    const num = nums[i];
+    countMap[num] = (countMap[num] || 0) + 1;
+    
+    if (countMap[num] > n / 2) {
+      return num;
     }
-
-    count += (num === candidate) ? 1 : -1;
   }
-
-  return candidate;
+  
+  return null;
 }
+
